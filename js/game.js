f9a1fa1204fd1a9ab6c7ac87b97696a316a6b946
Lev <leva1sokolov@gmail.com>
2024-02-13 22:10:12 +0700

    a bit of css improvements adding restart from space but at this time it make bugs, fix in process

--- js/game.js
+++ js/game.js
@@ -23,6 +23,7 @@ snake[0] = {
 };
 
 document.addEventListener("keydown", direction);
+document.addEventListener("keydown", restartFromSpace);
 
 let dir;
 let game;
@@ -32,6 +33,10 @@ function direction(event) {
   else if (event.keyCode == 38 && dir != "down") dir = "up";
   else if (event.keyCode == 39 && dir != "left") dir = "right";
   else if (event.keyCode == 40 && dir != "up") dir = "down";
+}
+
+function restartFromSpace(event) {
+  if (event.keyCode == 32) gameRestart();
 }
 
 function eatTail(head, arr) {
@@ -114,7 +119,7 @@ function gameRestart() {
   let modal = document.getElementById("modal");
   modal.setAttribute("class", "hidden");
 }
-const playAgainButton = document.getElementById("play-again");
+const playAgainButton = document.getElementById("play-again-btn");
 playAgainButton.addEventListener("click", gameRestart);
 
 gameStart();
