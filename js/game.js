42fbab314295f11199a8cf7cb1f7c52531406af7
Lev <leva1sokolov@gmail.com>
2024-02-24 19:57:06 +0700

    switched from PNG to my own canvas table, more beautiful counter

--- js/game.js
+++ js/game.js
@@ -1,9 +1,6 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-const ground = new Image();
-ground.src = "img/ground.png";
-
 const foodImg = new Image();
 foodImg.src = "img/apple.png";
 
@@ -12,34 +9,29 @@ const BOX = 32;
 let score = 0;
 
 let food = {
-  x: Math.floor(Math.random() * 17 + 1) * BOX,
-  y: Math.floor(Math.random() * 15 + 3) * BOX,
-};
-
-let snake = [];
-snake[0] = {
-  x: 9 * BOX,
-  y: 10 * BOX,
+  x: Math.floor(Math.random() * 16) * BOX,
+  y: Math.floor(Math.random() * 16) * BOX,
 };
 
 let dir;
 let game;
 let isBtnPressed;
+const field = 544;
+const scoreTable = document.getElementById("score-table");
 
 let onDirectionChange = (event) => {
   if (isBtnPressed) return;
-
-  if (event.keyCode == 37 && dir != "right") dir = "left";
-  else if (event.keyCode == 38 && dir != "down") dir = "up";
-  else if (event.keyCode == 39 && dir != "left") dir = "right";
-  else if (event.keyCode == 40 && dir != "up") dir = "down";
+  if (event.keyCode === 37 && dir !== "right") dir = "left";
+  else if (event.keyCode === 38 && dir !== "down") dir = "up";
+  else if (event.keyCode === 39 && dir !== "left") dir = "right";
+  else if (event.keyCode === 40 && dir !== "up") dir = "down";
   isBtnPressed = true;
 };
 document.addEventListener("keydown", onDirectionChange);
 
 let eatTail = (head, arr) => {
   for (let i = 0; i < arr.length; i++)
-    if (head.x == arr[i].x && head.y == arr[i].y) showGameOver();
+    if (head.x === arr[i].x && head.y === arr[i].y) showGameOver();
 };
 
 let showGameOver = () => {
@@ -50,48 +42,58 @@ let showGameOver = () => {
 };
 
 let drawGame = () => {
-  isBtnPressed = false;
-  ctx.drawImage(ground, 0, 0);
-
-  ctx.drawImage(foodImg, food.x, food.y);
-
-  for (let i = 0; i < snake.length; i++) {
-    ctx.fillStyle = i == 0 ? "limegreen" : "darkgreen";
-    ctx.fillRect(snake[i].x, snake[i].y, BOX, BOX);
-  }
-
-  ctx.fillStyle = "white";
-  ctx.font = "50px Arial";
-  ctx.fillText(score, BOX * 2.5, BOX * 1.7);
-
   let snakeHeadX = snake[0].x;
   let snakeHeadY = snake[0].y;
-
-  if (snakeHeadX == food.x && snakeHeadY == food.y) {
-    score++;
-    food = {
-      x: Math.floor(Math.random() * 17 + 1) * BOX,
-      y: Math.floor(Math.random() * 15 + 3) * BOX,
-    };
-  } else {
-    snake.pop();
-  }
-
   // CHECKING FOR OUT OF BOUNDS AND CALLING THE RESTART FUNCTION
   if (
-    snakeHeadX < BOX ||
-    snakeHeadX > BOX * 17 ||
-    snakeHeadY < 3 * BOX ||
-    snakeHeadY > BOX * 17
+    snakeHeadX < 0 ||
+    snakeHeadY < 0 ||
+    snakeHeadX > BOX * 16 ||
+    snakeHeadY > BOX * 16
   ) {
     showGameOver();
     return;
   }
 
-  if (dir == "left") snakeHeadX -= BOX;
-  if (dir == "right") snakeHeadX += BOX;
-  if (dir == "up") snakeHeadY -= BOX;
-  if (dir == "down") snakeHeadY += BOX;
+  isBtnPressed = false;
+  // Холст. Поле 17 на 17 кубов. 544px
+  canvas.width = field;
+  canvas.height = field;
+  // Закрашиваем внутреннюю область
+  ctx.fillStyle = "#75A828";
+  ctx.fillRect(0, 0, field, field);
+  // Закрашиваем квадратами поле в шахматном порядке
+  for (let i = 0; i < 17; i += 1)
+    for (let j = 0; j < 17; j += 1) {
+      if ((i + j * 17) % 2 === 0) {
+        ctx.fillStyle = "#92C34A";
+        ctx.fillRect(0 + i * 32, j * 32, 32, 32);
+      }
+    }
+
+  ctx.drawImage(foodImg, food.x, food.y);
+
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillStyle = i === 0 ? "limegreen" : "darkgreen";
+    ctx.fillRect(snake[i].x, snake[i].y, BOX, BOX);
+  }
+
+  if (snakeHeadX === food.x && snakeHeadY === food.y) {
+    score++;
+    food = {
+      x: Math.floor(Math.random() * 16) * BOX,
+      y: Math.floor(Math.random() * 16) * BOX,
+    };
+  } else {
+    snake.pop();
+  }
+
+  scoreTable.textContent = `Your score is ${score}`;
+
+  if (dir === "left") snakeHeadX -= BOX;
+  if (dir === "right") snakeHeadX += BOX;
+  if (dir === "up") snakeHeadY -= BOX;
+  if (dir === "down") snakeHeadY += BOX;
 
   const NEWHEAD = {
     x: snakeHeadX,
@@ -105,13 +107,13 @@ let drawGame = () => {
 
 let gameStart = () => {
   document.removeEventListener("keydown", gameStart);
-  let modal = document.getElementById("modal");
+  const modal = document.getElementById("modal");
   dir = undefined;
   score = 0;
   snake = [
     {
-      x: 9 * BOX,
-      y: 10 * BOX,
+      x: 8 * BOX,
+      y: 8 * BOX,
     },
   ];
   game = setInterval(drawGame, 100);
